055d9970b06900e5ec435c7bcc5965814f9dc65b
Mauricio Rivas <156487078+mriva077@users.noreply.github.com>
2025-03-04 16:18:47 -0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Tech on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriviaGameTests/TriviaGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameTests.swift
+//  TriviaGameTests
+//
+//  Created by Tech on 3/4/25.
+//
+
+import Testing
+@testable import TriviaGame
+
+struct TriviaGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
